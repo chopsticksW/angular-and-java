ad577215b80ad840f7bff0a3e5a70fb741216964
陆文钱 <luffybryant@gmail.com>
2016-03-07 23:59:25 +0800

    update

--- src/main/java/com/liuwenjian/controller/HelloController.java
+++ src/main/java/com/liuwenjian/controller/HelloController.java
@@ -4,14 +4,18 @@ package com.liuwenjian.controller;
  * Created by halapro on 2016/3/7.
  */
 
-//@Controller
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 class HelloController {
-//
-//    @RequestMapping("/hello")
-//    public ModelAndView hello(){
-//        ModelAndView mv =new ModelAndView();
-//        mv.addObject("spring", "spring mvc");
-//        mv.setViewName("hello");
-//        return mv;
-//    }
+
+    @RequestMapping("/hello")
+    public ModelAndView hello(){
+        ModelAndView mv =new ModelAndView();
+        mv.addObject("spring", "spring mvc");
+        mv.setViewName("hello");
+        return mv;
+    }
 }
