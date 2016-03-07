b97ca7c275191a5e2e70d774280d578a6733331a
陆文钱 <luffybryant@gmail.com>
2016-03-07 23:47:04 +0800

    update

---
+++ src/main/java/com/liuwenjian/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.liuwenjian.controller;
+
+/**
+ * Created by halapro on 2016/3/7.
+ */
+
+//@Controller
+class HelloController {
+//
+//    @RequestMapping("/hello")
+//    public ModelAndView hello(){
+//        ModelAndView mv =new ModelAndView();
+//        mv.addObject("spring", "spring mvc");
+//        mv.setViewName("hello");
+//        return mv;
+//    }
+}
