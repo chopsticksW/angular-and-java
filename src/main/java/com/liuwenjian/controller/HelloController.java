a4ae893a876fecbf6f30868543a23f6dcca97160
陆文钱 <luffybryant@gmail.com>
2016-03-08 09:45:40 +0800

    springmvc hello

--- src/main/java/com/liuwenjian/controller/HelloController.java
+++ src/main/java/com/liuwenjian/controller/HelloController.java
@@ -7,6 +7,7 @@ package com.liuwenjian.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 class HelloController {
