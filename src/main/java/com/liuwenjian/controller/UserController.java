eb4a0107a16232ee16575b225e7b2bd2ef3365d4
陆文钱 <luffybryant@gmail.com>
2016-03-08 17:52:36 +0800

    update

---
+++ src/main/java/com/liuwenjian/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.liuwenjian.controller;
+
+import com.sun.deploy.net.HttpResponse;
+import org.springframework.http.HttpRequest;
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by baozun on 2016/3/8.
+ */
+
+@Controller
+public class UserController {
+
+//    @RequestMapping(value="/login")
+//    public ModelAndView login(HttpRequest request, HttpResponse response) throws Exception {
+//        Map<String, Object> model = new HashMap<String, Object>();
+//        Gson gson = new Gson();
+//        return new ModelAndView(new JsonContentTypeView(gson.toJson(model)));
+//    }
+}
