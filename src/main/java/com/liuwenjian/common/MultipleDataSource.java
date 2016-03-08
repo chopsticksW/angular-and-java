e1e018ad787750ff6722e2aba5d073df8d289f5c
陆文钱 <luffybryant@gmail.com>
2016-03-08 17:02:34 +0800

    update

--- src/main/java/com/liuwenjian/common/MultipleDataSource.java
+++ src/main/java/com/liuwenjian/common/MultipleDataSource.java
@@ -4,7 +4,7 @@ import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 /**
  * Created by baozun on 2016/3/8.
- *
+ * 选择数据源
  */
 public class MultipleDataSource extends AbstractRoutingDataSource {
     private static final ThreadLocal<String> dataSourceKey = new InheritableThreadLocal<String>();
