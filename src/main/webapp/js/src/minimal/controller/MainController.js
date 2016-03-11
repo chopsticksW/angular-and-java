be3dd95fba7064f227b77ec117e1ebf5d48871b1
陆文钱 <luffybryant@gmail.com>
2016-03-11 12:27:54 +0800

    update

---
+++ src/main/webapp/js/src/minimal/controller/MainController.js
@@ -0,0 +1,8 @@
+/**
+ * Created by baozun on 2016/3/11.
+ */
+'use strict'
+var mainApp = angular.module("mainApp", ['ngRoute', 'ui.router']);
+mainApp.controller('mainController', ['$scope', function($scope) {
+
+}]);
---
+++ src/main/webapp/js/src/minimal/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by baozun on 2016/3/11.
+ */
+'use strict'
+var mainApp = angular.module("mainApp", ['ngRoute', 'ui.router']);
+mainApp.config(['$stateProvider', '$urlRouteProvider', '$locationProvider', function($stateProvider, $urlRouteProvider, $locationProvider) {
+
+    $urlRouteProvider.when('/userController/main', '/main');
+    $stateProvider
+        .state('main', {
+            url: '/main',
+            template: 'template/minimal/index.html',
+            controller: 'mainController'
+        });
+
+    if(window.history && history.pushState) {
+        $locationProvider.html5Mode(true);
+    }
+}]);
