0b9ccdc21cb7ba33bcce107174f41f78db753cd5
陆文钱 <luffybryant@gmail.com>
2016-03-11 15:21:55 +0800

    新增伪静态路由

--- src/main/webapp/js/src/minimal/controller/MainController.js
+++ src/main/webapp/js/src/minimal/controller/MainController.js
@@ -2,7 +2,8 @@
  * Created by baozun on 2016/3/11.
  */
 'use strict'
-var mainApp = angular.module("mainApp", ['ngRoute', 'ui.router']);
+var mainApp = angular.module("mainApp");
 mainApp.controller('mainController', ['$scope', function($scope) {
 
+    console.log(11);
 }]);
