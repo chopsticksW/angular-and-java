f10c9891c8867815d831d1faed308709532b6d31
halapro <luffybryant@gmail.com>
2016-04-04 23:33:59 +0800

    update

--- src/main/webapp/gulpfile.js
+++ src/main/webapp/gulpfile.js
@@ -2,10 +2,13 @@
  * Created by halapro on 2016/4/4.
  */
 var gulp    = require('gulp'),
+    jshint  = require('gulp-jshint'),
     uglify  = require('gulp-uglify');
 
 gulp.task('minify', function() {
-    gulp.src('js/src/minimal/*.js')
+    return gulp.src('js/src/minimal/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'))
         .pipe(uglify())
         .pipe(gulp.dest('dist'))
 });
